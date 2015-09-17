fda1db77b514af36bf063698e4dd52a29e441d14
JoaoBGusmao <joao@natsce.com>
2015-09-17 01:50:19 -0300

    GPL License

--- inc/classes/class.connection.php
+++ inc/classes/class.connection.php
@@ -1,31 +1,29 @@
 <?php
+/*
+	This file is part of MapleRoad
+	Copyright (C) 2015 João Gusmão
+	
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 	class connection extends PDO { 
-    /*
-    private $engine; 
-    private $host; 
-    private $database; 
-    private $user; 
-    private $pass; */
     
-    /*public function __construct(){ 
-        global $connection;
-		$this->engine = DB_ENGINE; 
-		$this->host = DB_HOST; 
-		$this->database = DB_DATABASE; 
-		$this->user = DB_USER; 
-		$this->pass = DB_PASS; 
-		$dns = $this->engine.':dbname='.$this->database.";host=".$this->host; 
-		$connection = parent::__construct( $dns, $this->user, $this->pass );
-    }
-	
-	public function getConnection() {
-		
-	}*/
 	private static $instance; 
 	
 	public function __construct() { 
-	// 
+	 
 	} 
 	public static function getInstance() {
 		if (!isset(self::$instance)) {
--- list_items.php
+++ list_items.php
@@ -1,6 +1,22 @@
-<!DOCTYPE html>
-
 <?php
+/*
+	This file is part of MapleRoad
+	Copyright (C) 2015 João Gusmão
+	
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 	require_once("./inc/config.php");
 	require_once("./inc/classes/class.item.php");
 	require_once("./inc/classes/class.mob.php");
@@ -8,7 +24,7 @@
 	
 	$searchEngine = new searchEngine;
 ?>
-
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
